21b54046adcd7c2a8f9fd0f0cdc2b7588f5ac14e
ioanarijnita <ioanarijnita@gmail.com>
2022-11-17 12:44:22 +0200

    added tema1

---
+++ Student.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+//Base Class
+
+class Uncopyable{
+    protected:
+        Uncopyable(){}
+        ~Uncopyable(){}
+    private:
+        Uncopyable(const Uncopyable&);
+        Uncopyable& operator=(const Uncopyable&);
+};
+
+class Student
+{                        
+    public: 
+    
+        Student(){} //default constructor
+    
+        Student(const int age, const int grade, const string& firstName, const string& lastName):     // custom paramaterized constructor
+             age(age),
+             grade(grade),
+             firstName(firstName),
+             lastName(lastName)
+       {}
+ 
+        void showStudent(){
+             cout<<"The student with name: "<<firstName<<" "<<lastName<<" and age: "<<age<<" has the following grade: "<<grade<<"\n";
+         }
+
+        ~Student(){ //destructor
+            cout<<"The student does not exist anymore in the memory.\n";
+        }
+
+     /* Custom Copy Operator, Item 6 */
+
+         Student& operator=(const Student& student){
+           this->age=student.age;
+           this->grade=student.grade;
+           this->firstName=student.firstName;
+           this->lastName=student.lastName;
+           return *this;
+         }
+    private: 
+
+        int age, grade;      
+        string firstName, lastName;
+};
+
+  
+int main()
+{
+     //Student student1(23, 10, "Ioana", "Rijnita"); 
+    //student1.showStudent();
+
+    //Student student2(student1);  // copy constructor 
+    // student2.showStudent();
+
+    //the copy constructor creates a separate memory block for the new object. But the assignment operator
+    // does not make new memory space
+
+     Student student3(24, 9, "Ioana2", "Rijnita2"); 
+     Student student4; 
+     student4=student3;  // copy assignment operator
+     
+     student4.showStudent();
+
+    return 0;
+}
---
+++ build/CMakeFiles/Student.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student.dir/Student.cpp.obj"
+  "CMakeFiles/Student.dir/Student.cpp.obj.d"
+  "Student.exe"
+  "Student.exe.manifest"
+  "Student.pdb"
+  "libStudent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
